d1cd999994ca11950eccb33b183e72a8f6412fed
Aadil Faisal <155349815+aadilfaisal@users.noreply.github.com>
2024-08-05 01:20:45 +0530

    Delete Basic Django Project directory

--- Basic Django Project/finalproj/finalapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Parent,Faculty,Transaction,Student
-# Register your models here.
-admin.site.register(Parent)
-
-admin.site.register(Transaction)
-admin.site.register(Student)
-admin.site.register(Faculty)
