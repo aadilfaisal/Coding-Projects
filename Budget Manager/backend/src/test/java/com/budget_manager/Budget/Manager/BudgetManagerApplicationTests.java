752f24730569a04b89cf102baac399bdcd21ff88
aadilfaisal <aadilkfaisal@gmail.com>
2026-05-21 19:17:28 +0400

    Added project structure

---
+++ Budget Manager/backend/src/test/java/com/budget_manager/Budget/Manager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget_manager.Budget.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
